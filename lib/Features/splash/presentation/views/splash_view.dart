c324c18cb3454f4437ce8f56351025c0b3d0e388
shahdeldissoky <149007002+shahdeldissoky@users.noreply.github.com>
2024-11-02 19:57:35 +0200

    firstcommit

---
+++ lib/Features/splash/presentation/views/splash_view.dart
@@ -0,0 +1,14 @@
+import 'package:book_app/Features/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: SplashViewBody(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:book_app/Features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BookApp());
+}
+
+class BookApp extends StatelessWidget {
+  const BookApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+   return const GetMaterialApp(
+    
+    home: SplashView(),
+   );
+  }
+}
