d8fdf46866345748f00d075dfedc9c1f9761c3e7
shahdeldissoky <149007002+shahdeldissoky@users.noreply.github.com>
2024-11-03 15:39:03 +0200

    custom appbar to home

---
+++ lib/Features/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:book_app/Features/home/presentation/view/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        CustomAppBar()
+      ],
+    );
+  }
+}
+
